611625ead6f38ba93e6e6f12ee651e6a00b151f0
okamodgi <nadakupnada@gmail.com>
2023-05-20 23:05:55 +0300

    laboratornaya8

--- src/Reader.java
+++ src/Reader.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Reader {
+import java.util.Scanner;
+public class Reader {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите ФИО: ");
+        String fio = in.nextLine();
+
+        System.out.print("Введите номер читательского билета: ");
+        int number = in.nextInt();
+
+        System.out.print("Введите факультет: ");
+        String facul = in.next();
+
+        System.out.print("Введите дату рождения: ");
+        String data = in.next();
+
+        System.out.print("Введите номер телефона: ");
+        int phone = in.nextInt();
+
+        System.out.print("Введите количество книг: ");
+        int kol = in.nextInt();
+
+        takeBook(fio);
+        takeBook(fio, kol);
+        returnBook(fio);
+        returnBook(fio, kol);
+    }
+    public static void takeBook(String f, int k){
+        int kol = k;
+        System.out.println(f + " взял " + k + " книги.");
+    }
+    public static void takeBook(String f){
+        System.out.println(f + " взял книги: Приключения, Словарь, Энциклопедия");
+    }
+    public static void returnBook(String f){
+        System.out.println(f + " вернул книги: Приключения, Словарь, Энциклопедия");
+    }
+    public static void returnBook(String f, int k ){
+        int kol = k;
+        System.out.println(f + " вернул " + k + " книги.");
+    }
 }
